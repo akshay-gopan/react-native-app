9d6ba45ab00c238b3e579d1e98fef0193c2ccf59
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-07-29 12:16:25 +0530

    images

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useAuth } from "../context/authProvider";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "../services/firebase";
@@ -7,6 +7,8 @@ import { db } from "../services/firebase";
 export default function ProfileScreen({ navigation }) {
   const { user, logout } = useAuth();
   const [userData, setUserData] = useState({ name: "", email: "" });
+
+  const imgUrl = 'https://avatar.iran.liara.run/public'
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -40,6 +42,13 @@ export default function ProfileScreen({ navigation }) {
       marginBottom: 20,
     },
 
+    profileimage: {
+        width: 150,
+        height: 150,
+        marginBottom: 20,
+  
+    },
+
     name: {
       fontSize: 20,
       fontWeight: "semibold",
@@ -70,6 +79,7 @@ export default function ProfileScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile</Text>
+      <Image source={{uri:'https://avatar.iran.liara.run/public'}} style = {styles.profileimage}/>
       <Text style={styles.name}>Name: {userData.name}</Text>
       <Text style={styles.email}>Email: {userData.email}</Text>
       <TouchableOpacity onPress={logout} style={styles.button}>
